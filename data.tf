5f3714f19f2aca24bd560c8ae24b6d1f604da81c
hassan-elhouari <elhouhass@gmail.com>
2026-06-04 20:40:53 +0000

    iam resources

---
+++ data.tf
@@ -0,0 +1,3 @@
+data "aws_s3_bucket" "bucket" {
+  bucket = "cmtr-3v98t79h-bucket-1780604812"
+}
--- storage.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "cmtr-3v98t79h-bucket-1780598335"
-
-  tags = {
-    Project = "cmtr-3v98t79h"
-  }
-}
