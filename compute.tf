720ac5713b3149c2109a4e723ad97c8bf895b893
hassan-elhouari <elhouhass@gmail.com>
2026-06-06 21:14:29 +0000

    + terraform fmt

--- compute.tf
+++ compute.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
-  ami            = var.ami_id
-  instance_type  = var.instance_type
-  subnet_id      = data.terraform_remote_state.base_infra.outputs.public_subnet_id
+  ami             = var.ami_id
+  instance_type   = var.instance_type
+  subnet_id       = data.terraform_remote_state.base_infra.outputs.public_subnet_id
   security_groups = [data.terraform_remote_state.base_infra.outputs.security_group_id]
 
   tags = {
